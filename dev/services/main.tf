1c05134e36c8748e284af96eb732a543ebbee7ff
Andre <p0gram3r@gmail.com>
2019-03-26 12:13:49 +0100

    making http server work

--- dev/services/main.tf
+++ dev/services/main.tf
@@ -12,27 +12,39 @@ variable "server_port" {
 
 
 resource "aws_instance" "example" {
-  ami = "ami-0f0debf49705e047c"
+  ami = "ami-0405a63f383fddd6b"
   instance_type = "t2.micro"
-
-  tags {
-    Name = "terraform-example"
-  }
+  vpc_security_group_ids = [
+    "${aws_security_group.instance.id}"
+  ]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p "${var.server_port}" &
               EOF
+
+
+  tags {
+    Name = "terraform-example"
+  }
 }
 
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
+
   ingress {
     from_port = "${var.server_port}"
     to_port = "${var.server_port}"
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
   }
+}
+
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
 }
